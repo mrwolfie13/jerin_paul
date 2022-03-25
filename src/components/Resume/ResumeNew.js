08700fbb0e971b3cf896a589d9219e2d373d7626
Jerin Paul <jerrymic2255@gmail.com>
2022-03-26 01:18:54 +0530

    resume added 3

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/mrwolfie13/jerry_Portfolio/master/src/Assets/resize.pdf";
+  "https://raw.githubusercontent.com/mrwolfie13/jerry_Portfolio/blob/0db1ae82bd970443924ece46e6563b025a067657/src/Assets/resize.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
